04db5f483a6209b8fa4b0120bd34e8071fe1e54c
Aditya <adityaavinashyadav@gmail.com>
2026-08-31 18:11:22 +0530

    cros config

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://employee-management-system-frontend-plum.vercel.app")
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
